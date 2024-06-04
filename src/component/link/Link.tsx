4be39323b8653216164ba687ea3a3521ac18ffce
jynxio <jinxiaomatrix@gmail.com>
2024-06-04 22:39:11 +0800

    feat: add link comp

---
+++ src/component/link/Link.tsx
@@ -0,0 +1,19 @@
+import css from './Link.module.css';
+import React from 'react';
+import NextLink from 'next/link';
+import clsx from 'clsx';
+
+type Props = React.ComponentProps<typeof NextLink>;
+
+const Link = React.forwardRef<HTMLAnchorElement, Props>(
+    ({ children, className, ...rest }: Props, ref) => {
+        return (
+            <NextLink ref={ref} className={clsx(css.container, className)} {...rest}>
+                {children}
+            </NextLink>
+        );
+    },
+);
+
+export { Link };
+export default Link;
