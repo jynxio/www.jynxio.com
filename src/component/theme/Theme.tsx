6397e10cab9f1f7560ffa7dc4cdae59567afa671
jynxio <jinxiaomatrix@gmail.com>
2024-06-04 22:38:39 +0800

    feat: add theme comp

---
+++ src/component/theme/Theme.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import React from 'react';
+import Icon from '@/component/icon';
+import css from './Theme.module.css';
+import * as Toggle from '@radix-ui/react-toggle';
+import { range } from 'lodash-es';
+import { Sparkles, MoonStar, Bot } from 'lucide-react';
+
+type TriState = 0 | 1 | 2;
+
+export default function () {
+    const [state, setState] = React.useState<TriState>(0);
+    const [isHydrated, setIsHydrated] = React.useState(false);
+    const styles = range(3).map(i => {
+        const style: Record<string, string> = {};
+
+        style.opacity = state === i ? '1' : '0';
+        isHydrated && (style.translate = state === i ? '0% 0%' : createRandomTranslate());
+
+        return style;
+    });
+
+    React.useEffect(() => setIsHydrated(true), []);
+
+    return (
+        <Toggle.Root aria-label="Toggle theme" className={css.container} onClick={handleClick}>
+            <section>
+                <Icon />
+            </section>
+
+            <section className={css.icon} style={styles[0]}>
+                <Icon label="Dark theme">
+                    <MoonStar />
+                </Icon>
+            </section>
+
+            <section className={css.icon} style={styles[1]}>
+                <Icon label="Auto theme">
+                    <Bot />
+                </Icon>
+            </section>
+
+            <section className={css.icon} style={styles[2]}>
+                <Icon label="Light theme">
+                    <Sparkles />
+                </Icon>
+            </section>
+        </Toggle.Root>
+    );
+
+    function handleClick() {
+        setState(prev => ((prev + 1) % 3) as TriState);
+    }
+}
+
+function createRandomTranslate() {
+    const xSign = randPlusMinusZero();
+    const ySign = xSign === 0 ? randPlusMinus() : randPlusMinusZero();
+
+    return `${xSign * 100}% ${ySign * 100}%`;
+
+    function randPlusMinus() {
+        const values = [1, -1] as const;
+        const randomIndex = Math.floor(Math.random() * values.length);
+        return values[randomIndex];
+    }
+
+    function randPlusMinusZero() {
+        const values = [1, -1, 0] as const;
+        const randomIndex = Math.floor(Math.random() * values.length);
+        return values[randomIndex];
+    }
+}
