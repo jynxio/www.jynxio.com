27cb2eb1dd68c5484cb4887baa37676f652bdb1c
jynxio <jinxiaomatrix@gmail.com>
2024-06-24 22:41:15 +0800

    perf: improve search comp

--- src/component/search/Search.tsx
+++ src/component/search/Search.tsx
@@ -1,39 +1,35 @@
-'use client';
+"use client";
 
-import React from 'react';
-import css from './Search.module.css';
-import Button from '@/component/button';
-import localFont from 'next/font/local';
-import clsx from 'clsx';
-import Dialog from '@/component/dialog';
-import Icon from '@/component/icon';
-import Link from '@/component/link';
-import { X } from 'lucide-react';
-import { range } from 'lodash-es';
-import { useEvent } from 'react-use';
+import React from "react";
+import css from "./Search.module.css";
+import Button from "@/component/button";
+import localFont from "next/font/local";
+import clsx from "clsx";
+import Dialog from "@/component/dialog";
+import Icon from "@/component/icon";
+import Link from "@/component/link";
+import { X } from "lucide-react";
+import { range } from "lodash-es";
+import { useEvent } from "react-use";
 
 const interRegularSubset = localFont({
-    src: './inter-regular-subset.woff2',
-    display: 'swap',
-    weight: 'normal',
-    style: 'normal',
+    src: "./inter-regular-subset.woff2",
+    display: "swap",
+    weight: "normal",
+    style: "normal",
 });
 
-const fakePostNames = range(8).map(i => ({ uuid: crypto.randomUUID(), name: `post-${i}` }));
+const fakePostNames = range(8).map((i) => ({ uuid: crypto.randomUUID(), name: `post-${i}` }));
 
 export default function Search() {
     const linkRefs = React.useRef<(HTMLAnchorElement | null)[]>([]); // TODO: 如果post更新了，那么就需要清空该数组，否则会造成内存泄漏
 
-    const [text, setText] = React.useState('');
+    const [text, setText] = React.useState("");
     const [selected, setSelected] = React.useState(-1);
     const [visible, setVisible] = React.useState(false);
-    const MemoOnKeydown = React.useCallback<(e: KeyboardEvent) => void>(onKeydown, [
-        selected,
-        visible,
-    ]);
 
-    useEvent('keydown', (e: Event) => MemoOnKeydown(e as KeyboardEvent), globalThis);
-    React.useEffect(() => setSelected(-1), [visible]);
+    useEvent("keydown", (e: Event) => onKeydown(e as KeyboardEvent), globalThis);
+    React.useEffect(() => void (visible && setSelected(-1)), [visible]);
 
     return (
         <Dialog.Root open={visible} onOpenChange={setVisible}>
@@ -50,15 +46,15 @@ export default function Search() {
                         className={css.input}
                         placeholder="Type to search..."
                         value={text}
-                        onChange={e => setText(e.target.value)}
+                        onChange={(e) => setText(e.target.value)}
                     />
                     <Button
                         label="close"
                         className={css.close}
                         onClick={() => setVisible(false)}
-                        onKeyDown={e => e.stopPropagation()}
+                        onKeyDown={(e) => e.stopPropagation()}
                     >
-                        <Icon label="close" size={16}>
+                        <Icon label="close" width={16}>
                             <X />
                         </Icon>
                     </Button>
@@ -72,12 +68,12 @@ export default function Search() {
                     ) : (
                         <ul>
                             {fakePostNames.map(({ uuid, name }, i) => (
-                                <li key={uuid} className={i === selected ? css.selected : ''}>
+                                <li key={uuid} className={i === selected ? css.selected : ""}>
                                     <Link
                                         href={`/${name}`}
                                         tabIndex={-1}
                                         className={css.link}
-                                        ref={e => void (linkRefs.current[i] = e)}
+                                        ref={(e) => void (linkRefs.current[i] = e)}
                                         onMouseEnter={() => setSelected(i)}
                                     >
                                         {name}
@@ -93,26 +89,26 @@ export default function Search() {
 
     function onKeydown(e: KeyboardEvent) {
         switch (e.key.toLowerCase()) {
-            case '/':
+            case "/":
                 visible || e.preventDefault();
                 setVisible(true);
                 break;
 
-            case 'k':
-                e.metaKey && setVisible(prev => !prev);
+            case "k":
+                e.metaKey && setVisible((prev) => !prev);
                 break;
 
-            case 'arrowup':
+            case "arrowup":
                 e.preventDefault();
-                setSelected(prev => (prev + fakePostNames.length - 1) % fakePostNames.length);
+                setSelected((prev) => (prev + fakePostNames.length - 1) % fakePostNames.length);
                 break;
 
-            case 'arrowdown':
+            case "arrowdown":
                 e.preventDefault();
-                setSelected(prev => (prev + 1) % fakePostNames.length);
+                setSelected((prev) => (prev + 1) % fakePostNames.length);
                 break;
 
-            case 'enter':
+            case "enter":
                 linkRefs.current[selected]?.click();
                 setVisible(false);
                 break;
