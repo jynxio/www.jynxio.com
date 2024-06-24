707f7320f5fc4aefc9e5e206d737aeb4558d2893
jynxio <jinxiaomatrix@gmail.com>
2024-06-24 22:40:57 +0800

    feat: add icon comp

--- src/component/icon/Icon.tsx
+++ src/component/icon/Icon.tsx
@@ -1,32 +1,37 @@
-'use client';
+"use client";
 
-import css from './Icon.module.css';
-import React from 'react';
-import * as AccessibleIcon from '@radix-ui/react-accessible-icon';
+import css from "./Icon.module.css";
+import React from "react";
+import * as AccessibleIcon from "@radix-ui/react-accessible-icon";
+import clsx from "clsx";
 
 // 代表仅作为占位符来使用
-type PlaceholderProps = {
+interface PlaceholderProps extends React.HTMLAttributes<HTMLDivElement> {
     label: string;
+    width?: number;
+    height?: number;
     children: React.ReactNode;
-    size?: number;
-};
-type IconProps = {
+}
+interface IconProps extends React.HTMLAttributes<HTMLDivElement> {
     label?: never;
     children?: never;
-    size?: number;
-} & React.HTMLAttributes<HTMLDivElement>;
+    width?: number;
+    height?: number;
+}
+
 type Props = PlaceholderProps | IconProps;
 
 const Icon = React.forwardRef<HTMLDivElement, Props>(
-    ({ label, size = 18, children }: Props, ref) => {
+    ({ label, width, height, children, className, style, ...rest }: Props, ref) => {
+        const blockSize = height === undefined ? "fit-content" : `calc(${height} / 16 * 1rem)`;
+        const inlineSize = width === undefined ? "fit-content" : `calc(${width}  / 16 * 1rem)`;
+
         return (
             <div
                 ref={ref}
-                className={css.container}
-                style={{
-                    blockSize: `calc(${size} / 16 * 1rem)`,
-                    inlineSize: `calc(${size} / 16 * 1rem)`,
-                }}
+                className={clsx(css.container, className)}
+                style={{ blockSize, inlineSize, ...style }}
+                {...rest}
             >
                 {label && <AccessibleIcon.Root label={label}>{children}</AccessibleIcon.Root>}
             </div>
