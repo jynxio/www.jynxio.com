8cd5e32f63e63b19c3524ec0c4276446a552ac93
jynxio <jinxiaomatrix@gmail.com>
2024-06-24 22:42:58 +0800

    perf: improve theme control comp

--- src/component/theme/Theme.tsx
+++ src/component/theme/Theme.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Icon from '@/component/icon';
-import css from './Theme.module.css';
-import * as Toggle from '@radix-ui/react-toggle';
-import { range } from 'lodash-es';
-import { Sparkles, MoonStar, Bot } from 'lucide-react';
+import React from "react";
+import Icon from "@/component/icon";
+import css from "./Theme.module.css";
+import * as Toggle from "@radix-ui/react-toggle";
+import { range } from "lodash-es";
+import { Sparkles, MoonStar, Bot } from "lucide-react";
 
 type TriState = 0 | 1 | 2;
 
 export default function () {
     const [state, setState] = React.useState<TriState>(0);
     const [isHydrated, setIsHydrated] = React.useState(false);
-    const styles = range(3).map(i => {
-        const style: Record<string, string> = {};
-
-        style.opacity = state === i ? '1' : '0';
-        isHydrated && (style.translate = state === i ? '0% 0%' : createRandomTranslate());
+    const styles = range(3).map((i) => {
+        const style: Record<string, string> = {
+            opacity: state === i ? "1" : "0",
+            translate: isHydrated ? (state === i ? "0% 0%" : createRandomTranslate()) : "",
+        };
 
         return style;
     });
@@ -26,23 +26,23 @@ export default function () {
     return (
         <Toggle.Root aria-label="Toggle theme" className={css.container} onClick={handleClick}>
             <section>
-                <Icon />
+                <Icon width={28} />
             </section>
 
             <section className={css.icon} style={styles[0]}>
-                <Icon label="Dark theme">
+                <Icon label="Dark theme" width={28}>
                     <MoonStar />
                 </Icon>
             </section>
 
             <section className={css.icon} style={styles[1]}>
-                <Icon label="Auto theme">
+                <Icon label="Auto theme" width={28}>
                     <Bot />
                 </Icon>
             </section>
 
             <section className={css.icon} style={styles[2]}>
-                <Icon label="Light theme">
+                <Icon label="Light theme" width={28}>
                     <Sparkles />
                 </Icon>
             </section>
@@ -50,7 +50,7 @@ export default function () {
     );
 
     function handleClick() {
-        setState(prev => ((prev + 1) % 3) as TriState);
+        setState((prev) => ((prev + 1) % 3) as TriState);
     }
 }
 
