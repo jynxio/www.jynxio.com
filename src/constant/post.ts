58b5b90603b9e8f35b67bfc43c86f3e7c90babaa
jynxio <jinxiaomatrix@gmail.com>
2024-06-24 22:47:24 +0800

    chore: add constant data

---
+++ src/constant/post.ts
@@ -0,0 +1,5 @@
+const POST_DIR_PATH = "/post";
+const IMAGE_HOSTING_DIR_PATH = "/public/image-hosting";
+
+export { POST_DIR_PATH, IMAGE_HOSTING_DIR_PATH };
+export default { POST_DIR_PATH, IMAGE_HOSTING_DIR_PATH };
