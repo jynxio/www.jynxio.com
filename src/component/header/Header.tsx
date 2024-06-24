40e05b0099f07f29bd05034d70291762f320b9f4
jynxio <jinxiaomatrix@gmail.com>
2024-06-24 22:37:39 +0800

    feat: add header comp

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,51 +1,21 @@
-import css from './Header.module.css';
+import css from "./Header.module.css";
+import clsx from "clsx";
+import React from "react";
+import Link from "@/component/link";
 
-import React from 'react';
-import Search from '@/component/search';
-import Theme from '@/component/theme';
-import Link from '@/component/link';
-import * as Avatar from '@/component/avatar';
-import Icon from '@/component/icon';
-import avatar from '@/asset/jynxio-avatar.png';
-import { Github, Twitter, Rss } from 'lucide-react';
+type Props = Omit<React.HTMLAttributes<HTMLHeadElement>, "children">;
 
-export default function Header() {
+const Header = React.forwardRef<HTMLHeadElement, Props>(({ className, ...rest }, ref) => {
     return (
-        <div className={css.container}>
-            <section className={css.avatar}>
-                <Avatar.Root>
-                    <Avatar.Image src={avatar.src} alt="Jynxio's avatar" width={20} height={20} />
-                    {/* FIXME: 填充 fallback */}
-                    <Avatar.Fallback>Fallback</Avatar.Fallback>
-                </Avatar.Root>
-            </section>
-
-            <section className={css.chapter}>
+        <header className={clsx(css.container, className)} ref={ref} {...rest}>
+            <nav>
                 <Link href="/">Home</Link>
-                <Link href="/blog">Blog</Link>
-            </section>
-
-            <section className={css.control}>
-                <Search />
-
-                <Link href="https://github.com/jynxio" target="_blank">
-                    <Icon label="github">
-                        <Github />
-                    </Icon>
-                </Link>
-                <Link href="https://x.com/jyn_xio" target="_blank">
-                    <Icon label="twitter">
-                        <Twitter />
-                    </Icon>
-                </Link>
-                <Link href="/rss" target="_blank">
-                    <Icon label="rss">
-                        <Rss />
-                    </Icon>
-                </Link>
-
-                <Theme />
-            </section>
-        </div>
+                <Link href="/post">Post</Link>
+                <Link href="/inder">Idea</Link>
+            </nav>
+        </header>
     );
-}
+});
+
+export { Header };
+export default Header;
