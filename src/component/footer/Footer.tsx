53c156bfc84a59c40c7a6f76bf9204a9b1045b21
jynxio <jinxiaomatrix@gmail.com>
2024-06-24 22:40:02 +0800

    feat: add footer comp

---
+++ src/component/footer/Footer.tsx
@@ -0,0 +1,38 @@
+import "@/util/font";
+import css from "./Footer.module.css";
+import Search from "@/component/search";
+import Theme from "@/component/theme";
+import Link from "@/component/link";
+import Icon from "@/component/icon";
+import { Github, Twitter, Rss } from "lucide-react";
+
+function Footer() {
+    return (
+        <footer className={css.container}>
+            <address>© 2022-2024 Jynxio | CC BY-NC-ND 4.0</address>
+
+            <nav>
+                <Link href="https://github.com/jynxio" target="_blank">
+                    <Icon label="github">
+                        <Github />
+                    </Icon>
+                </Link>
+
+                <Link href="https://x.com/jyn_xio" target="_blank">
+                    <Icon label="twitter">
+                        <Twitter />
+                    </Icon>
+                </Link>
+
+                <Link href="/rss" target="_blank">
+                    <Icon label="rss">
+                        <Rss />
+                    </Icon>
+                </Link>
+            </nav>
+        </footer>
+    );
+}
+
+export { Footer };
+export default Footer;
