d8ad8e58ed4e92151e034d849613e5a3e80151e5
jynxio <jinxiaomatrix@gmail.com>
2024-06-29 21:41:55 +0800

    perf: improve the mdxremote components styles

--- src/layout/mdx/Li.tsx
+++ src/layout/mdx/Li.tsx
@@ -1,7 +1,14 @@
+import css from "./Li.module.css";
+import clsx from "clsx";
+
 type Props = React.DetailedHTMLProps<React.LiHTMLAttributes<HTMLLIElement>, HTMLLIElement>;
 
-function Li({ children, ...rest }: Props) {
-    return <li {...rest}>{children}</li>;
+function Li({ children, className, ...rest }: Props) {
+    return (
+        <li className={clsx(css.container, className)} {...rest}>
+            {children}
+        </li>
+    );
 }
 
 export { Li };
--- src/layout/mdx/Ul.tsx
+++ src/layout/mdx/Ul.tsx
@@ -1,7 +1,14 @@
+import css from "./Ul.module.css";
+import clsx from "clsx";
+
 type Props = React.DetailedHTMLProps<React.HTMLAttributes<HTMLUListElement>, HTMLUListElement>;
 
-function Ul({ children, ...rest }: Props) {
-    return <ul {...rest}>{children}</ul>;
+function Ul({ children, className, ...rest }: Props) {
+    return (
+        <ul className={clsx(css.container, className)} {...rest}>
+            {children}
+        </ul>
+    );
 }
 
 export { Ul };
