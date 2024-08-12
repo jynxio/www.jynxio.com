cae1ee9fc25209545848eb9c3d3cbfc25db95507
jynxio <jinxiaomatrix@gmail.com>
2024-08-12 20:31:30 +0800

    feat: remove bio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import css from "./page.module.css";
-import Bio from "@/layout/bio";
 import Hero from "@/layout/hero";
 
 function Page() {
     return (
         <main className={css.container}>
-            <Bio />
             <Hero />
         </main>
     );
