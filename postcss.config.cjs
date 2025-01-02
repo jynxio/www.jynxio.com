a7b54e99629596678d4ad7937c5bd69a1782f07c
jynxio <jinxiaomatrix@gmail.com>
2025-01-02 22:44:41 +0800

    refactor: Scaffold

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: {
+        'autoprefixer': {},
+        'postcss-nesting': {},
+        'postcss-preset-env': {},
+        '@csstools/postcss-light-dark-function': {},
+    },
+};
