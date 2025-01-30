6bdcd2753cfd785abbc3b7b0904edf4f01c559b4
jynxio <jinxiaomatrix@gmail.com>
2025-01-30 15:39:06 +0800

    refactor: Update article list sorting algorithm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@ import { reqList } from '@/helper/post';
 import List from './_comp/post-list';
 
 async function Blog() {
-    return <List list={await reqList()} />;
+    const list = await reqList();
+    const orderedList = list.toSorted((a, b) => {
+        const timestampA = Number(new Date(a.publishedDate));
+        const timestampB = Number(new Date(b.publishedDate));
+
+        return timestampB - timestampA;
+    });
+
+    return <List list={orderedList} />;
 }
 
 export default Blog;
