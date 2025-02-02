30a4bae85f6f15176eae344a2dc48e76ec753190
jynxio <jinxiaomatrix@gmail.com>
2025-02-02 21:35:46 +0800

    perf: Lower keyboard navigation priority for back btn

--- src/app/post/[slug]/layout.tsx
+++ src/app/post/[slug]/layout.tsx
@@ -3,19 +3,19 @@ import type React from 'react';
 import Icon from '@/component/icon';
 import Link from '@/component/link';
 import { Undo2 } from 'lucide-react';
-import css from './layout.module.css';
+import css from './layout.module.scss';
 
 function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <div>
+            {children}
+
             <nav className={css.nav}>
                 <Link className={css.link} href="/">
                     <Icon label="return" fontSize="1.2rem" icon={<Undo2 />} />
                     Return
                 </Link>
             </nav>
-
-            {children}
         </div>
     );
 }
