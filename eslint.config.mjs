5cc7b96c7f4a617fff6f01fdd1f1eaada34204bd
jynxio <jinxiaomatrix@gmail.com>
2025-04-18 22:32:02 +0800

    fix: ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,21 @@
-import { FlatCompat } from '@eslint/eslintrc';
 import pluginVanilla from '@eslint/js';
+import { flatConfig as pluginNext } from '@next/eslint-plugin-next';
 import pluginPrettier from 'eslint-config-prettier';
+import pluginReact from 'eslint-plugin-react';
 import pluginReactCompiler from 'eslint-plugin-react-compiler';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import pluginTypescript from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const compat = new FlatCompat({ baseDirectory: __dirname });
+const pluginReactWrapper = {
+    settings: { react: { version: 'detect' } },
+    ...pluginReact.configs.flat.recommended,
+};
+const pluginReactHooksWrapper = {
+    plugins: { 'react-hooks': pluginReactHooks },
+    rules: pluginReactHooks.configs.recommended.rules,
+};
 
 const pluginReactCompilerWrapper = {
     plugins: { 'react-compiler': pluginReactCompiler },
@@ -17,24 +23,41 @@ const pluginReactCompilerWrapper = {
 };
 
 const eslintConfig = [
-    {
-        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-        ignores: ['**/.next/', '**/out/', '**/dist/', '**/.build/', '**/.vercel/'],
-        languageOptions: { globals: { ...globals.browser, ...globals.node } },
-    },
+    /**
+     * Base
+     */
+    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    { ignores: ['**/.next/', '**/out/', '**/dist/', '**/.build/', '**/.vercel/'] },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
 
+    /**
+     * Plugin
+     */
     pluginVanilla.configs.recommended,
-    pluginPrettier,
-    ...pluginTypescript.configs.recommended,
-    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    pluginReactWrapper,
+    pluginReactHooksWrapper,
+    pluginReactRefresh.configs.recommended,
     pluginReactCompilerWrapper,
+    pluginNext.coreWebVitals, // Refer to https://github.com/vercel/next.js/blob/canary/packages/eslint-plugin-next/src/index.ts
+    ...pluginTypescript.configs.recommended,
+    pluginPrettier,
 
+    /**
+     * Overrides
+     */
     {
         rules: {
             'no-unused-expressions': 'off',
             'react/react-in-jsx-scope': 'off',
             'import/no-anonymous-default-export': 'off',
             '@typescript-eslint/no-unused-expressions': 'off',
+        },
+    },
+
+    {
+        files: ['**/page.tsx', '**/layout.tsx'],
+        rules: {
+            'react-refresh/only-export-components': 'off',
         },
     },
 ];
