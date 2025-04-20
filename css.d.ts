2ee292fc2be1935701f68e2084365aab6671ad80
jynxio <jinxiaomatrix@gmail.com>
2025-04-20 22:29:08 +0800

    chore: Improve css.d.ts

--- css.d.ts
+++ css.d.ts
@@ -1,4 +1,11 @@
-declare module '*.css' {
-    const content: { [className: string]: string };
-    export default content;
+declare module '*.css';
+declare module '*.module.css' {
+    const classes: { readonly [key: string]: string };
+    export default classes;
+}
+
+declare module '*.scss';
+declare module '*.module.scss' {
+    const classes: { readonly [key: string]: string };
+    export default classes;
 }
