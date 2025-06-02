713e6ba0ca4ab02df969527b742bf2c24b316126
jynxio <jinxiaomatrix@gmail.com>
2025-06-02 18:51:18 +0800

    feat: Add animation to Posts page

---
+++ src/app/posts/list/fade.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { range } from 'lodash-es';
+import { motion, useAnimate } from 'motion/react';
+import { useEffect, type PropsWithChildren } from 'react';
+import { useInView } from 'react-intersection-observer';
+
+const initialMotion = { scale: 0.9, opacity: 0 } as const;
+
+function Fade({ children }: PropsWithChildren) {
+    const [ref1, animate] = useAnimate<HTMLDivElement>();
+    const { ref: ref2, entry } = useInView({ threshold: range(101).map(item => item / 100) });
+
+    useEffect(() => {
+        ref2(ref1.current);
+        return () => ref2(null);
+    }, [ref1, ref2]);
+
+    useEffect(() => {
+        const isIntersected = entry?.isIntersecting;
+        if (!isIntersected) return;
+
+        const ratio = entry.intersectionRatio;
+        const { scale, opacity } = initialMotion;
+        const position = entry.intersectionRect.top === 0 ? 'top' : 'bottom';
+        const nextMotion = {
+            opacity: opacity + ratio * (1 - opacity),
+            scale: position === 'top' ? 1 : scale + ratio * (1 - scale),
+        };
+
+        animate(ref1.current, nextMotion, { duration: 0.1 });
+    }, [entry, animate, ref1]);
+
+    return (
+        <motion.div ref={ref1} initial={initialMotion}>
+            {children}
+        </motion.div>
+    );
+}
+
+export { Fade };
--- src/app/posts/list/list.tsx
+++ src/app/posts/list/list.tsx
@@ -1,37 +1,28 @@
-import type { ReactNode } from 'react';
-
 import { reqList } from '@/helpers/posts';
+import { Fade } from './fade';
 import { Item } from './item';
+
 import css from './list.module.css';
 
-type Props = Readonly<{ list: Awaited<ReturnType<typeof reqList>> }>;
+type Props = { list: Awaited<ReturnType<typeof reqList>> };
 
-function List({ list }: Props) {
-    type T = { uuid: string; elem: ReactNode }[];
-
-    const items = list.reduce<T>((acc, { slug, title, abstract }, idx) => {
-        const href = `/posts/${slug}`;
-        const props = { title, abstract, href };
-
-        idx === 0 || acc.push({ uuid: `${slug}-divider`, elem: <Divider /> });
-        acc.push({ uuid: slug, elem: <Item {...props} /> });
-
-        return acc;
-    }, []);
-
+function List({ list }: Readonly<Props>) {
     return (
         <ul className={css.list}>
-            {items.map(({ uuid, elem }) => (
-                <li key={uuid} className={css.item}>
-                    {elem}
-                </li>
-            ))}
+            {list.map(({ slug, title, abstract }) => {
+                const href = `/posts/${slug}`;
+                const props = { title, abstract, href };
+
+                return (
+                    <li key={slug} className={css.item}>
+                        <Fade>
+                            <Item {...props} />
+                        </Fade>
+                    </li>
+                );
+            })}
         </ul>
     );
-}
-
-function Divider() {
-    return <div className={css.divider}>/ / /</div>;
 }
 
 export { List };
