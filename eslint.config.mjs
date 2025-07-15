c48bd06a8eb3ad25e677018deacb651b64c7a718
jynxio <jinxiaomatrix@gmail.com>
2025-07-15 23:23:43 +0800

    chore: Update scaffold configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,62 +1,53 @@
-import pluginVanilla from '@eslint/js';
-import pluginJynxio from '@jynxio/eslint-plugin';
-import { flatConfig as pluginNext } from '@next/eslint-plugin-next';
-import pluginPrettier from 'eslint-config-prettier';
-import pluginReact from 'eslint-plugin-react';
-import pluginReactCompiler from 'eslint-plugin-react-compiler';
-import pluginReactHooks from 'eslint-plugin-react-hooks';
-import pluginReactRefresh from 'eslint-plugin-react-refresh';
+import vanilla from '@eslint/js';
+import jynxio from '@jynxio/eslint-plugin';
+import { flatConfig as next } from '@next/eslint-plugin-next';
+import prettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
+import reactCompiler from 'eslint-plugin-react-compiler';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import path from 'node:path';
-import pluginTypescript from 'typescript-eslint';
-
-const pluginReactWrapper = {
-    settings: { react: { version: 'detect' } },
-    ...pluginReact.configs.flat.recommended,
-};
-const pluginReactHooksWrapper = {
-    plugins: { 'react-hooks': pluginReactHooks },
-    rules: pluginReactHooks.configs.recommended.rules,
-};
-
-const pluginReactCompilerWrapper = {
-    plugins: { 'react-compiler': pluginReactCompiler },
-    rules: { 'react-compiler/react-compiler': 'error' },
-};
+import typescript from 'typescript-eslint';
 
 export default defineConfig([
     /**
-     * Base
+     * Global Configuration
      */
     { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
     { ignores: ['**/.next/', '**/out/', '**/dist/', '**/.build/', '**/.vercel/'] },
     { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
 
     /**
-     * Plugin
+     * Vanilla JS/TS
      */
-    pluginVanilla.configs.recommended,
-    pluginReactWrapper,
-    pluginReactHooksWrapper,
-    pluginReactRefresh.configs.recommended,
-    pluginReactCompilerWrapper,
-    pluginNext.coreWebVitals, // Refer to https://github.com/vercel/next.js/blob/canary/packages/eslint-plugin-next/src/index.ts
-    ...pluginTypescript.configs.recommended,
-    pluginPrettier,
-
-    /**
-     * Overrides
-     */
+    vanilla.configs.recommended,
+    ...typescript.configs.recommended,
     {
         rules: {
             'no-unused-expressions': 'off',
-            'react/react-in-jsx-scope': 'off',
             'import/no-anonymous-default-export': 'off',
             '@typescript-eslint/no-unused-expressions': 'off',
         },
     },
 
+    /**
+     * React
+     */
+    {
+        settings: { react: { version: 'detect' } },
+        ...react.configs.flat.recommended,
+    },
+    {
+        plugins: { 'react-compiler': reactCompiler },
+        rules: { 'react-compiler/react-compiler': 'error' },
+    },
+    {
+        plugins: { 'react-hooks': reactHooks },
+        rules: reactHooks.configs.recommended.rules,
+    },
+    reactRefresh.configs.recommended,
     {
         files: ['**/page.tsx', '**/layout.tsx'],
         rules: {
@@ -64,8 +55,22 @@ export default defineConfig([
         },
     },
     {
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+        },
+    },
+
+    /**
+     * Next
+     */
+    next.coreWebVitals, // Refer to https://github.com/vercel/next.js/blob/canary/packages/eslint-plugin-next/src/index.ts
+
+    /**
+     * Jynxio
+     */
+    {
         plugins: {
-            jynxio: pluginJynxio,
+            jynxio,
         },
         rules: {
             'jynxio/underscore-file-pattern': [
@@ -77,4 +82,9 @@ export default defineConfig([
             ],
         },
     },
+
+    /**
+     * Prettier
+     */
+    prettier,
 ]);
