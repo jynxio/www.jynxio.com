000584edb37373984cd86b05db2f7e477534bd1b
jynxio <jinxiaomatrix@gmail.com>
2025-10-21 02:43:10 +0800

    chore: Update todo

--- app/_comps/mdx/_comps/img.tsx
+++ app/_comps/mdx/_comps/img.tsx
@@ -1,13 +1,6 @@
 import clsx from 'clsx';
 import css from './img.module.css';
 
-/**
- * TODO:
- * next/image不支持SSG，除非我需要自定义优化器（详见：https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#image-optimization），
- * 但是，next/image需要传入明确的width和height，而这是不可能的，因此next/image或许根本就不适合此组件。如果你只是想要做图像压缩，那么可以使用Vite插件。
- *
- * Unpic似乎是一个很棒Image组件！See：https://unpic.pics/
- */
 type Props = Readonly<React.ImgHTMLAttributes<HTMLImageElement> & { postSlug: string }>;
 type ImgCtx = {
     src: string;
@@ -19,7 +12,7 @@ type ImgCtx = {
 };
 
 async function Img({ alt, src: originalSrc, className, postSlug, ...rest }: Props) {
-    if (!originalSrc) return;
+    if (typeof originalSrc !== 'string') return;
 
     const name = originalSrc.split('/').at(-1);
     if (!name) return;
@@ -27,6 +20,13 @@ async function Img({ alt, src: originalSrc, className, postSlug, ...rest }: Prop
     const imgModule = await import(`$/post/${postSlug}/img/${name}`);
     const { src } = imgModule.default as ImgCtx;
 
+    /**
+     * @todo
+     * next/image不支持SSG，除非我需要自定义优化器（详见：https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#image-optimization），
+     * 但是，next/image需要传入明确的width和height，而这是不可能的，因此next/image或许根本就不适合此组件。如果你只是想要做图像压缩，那么可以使用Vite插件。
+     *
+     * Unpic似乎是一个很棒Image组件！See：https://unpic.pics/
+     */
     return <img {...rest} src={src} alt={alt ?? ''} className={clsx(css.container, className)} />;
 }
 
