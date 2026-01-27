1aec3116cf4a6b55525f885f29dc78e9d5418833
jynxio <jinxiaomatrix@gmail.com>
2026-01-27 23:56:25 +0800

    fix: Sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,11 +7,11 @@ export const dynamic = 'force-static';
 async function sitemap() {
     const list = await reqList();
     const blogs = list.map(item => ({
-        url: new URL(path.join('blog', item.slug), APP_URL),
+        url: new URL(path.join('posts', item.slug), APP_URL),
         lastModified: new Date(item.updatedDate).toISOString().split('T')[0],
     }));
 
-    const routes = ['', '/blog'].map(route => ({
+    const routes = ['', '/posts'].map(route => ({
         url: new URL(route, APP_URL),
         lastModified: new Date().toISOString().split('T')[0],
     }));
